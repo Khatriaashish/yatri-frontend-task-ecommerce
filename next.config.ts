948d7f921dfb0ea095002dda566f9acf1a509ca0
Ashish <khatriaashish1@gmail.com>
2025-08-29 02:05:26 +0545

    :up: update: whitelist images from google and github

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["fakestoreapi.com", "avatars.githubusercontent.com"],
+    domains: [
+      "fakestoreapi.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
